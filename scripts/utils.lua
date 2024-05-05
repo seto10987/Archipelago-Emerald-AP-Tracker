98cc029167f55257dffe54f9817ebcfc4633cbf7
Steven <stevenyates0705@gmail.com>
2024-05-05 16:47:17 -0500

    make hosted items toggle when the item is selected in the item grids

--- scripts/utils.lua
+++ scripts/utils.lua
@@ -37,6 +37,19 @@ function dump_table(o, depth)
 	end
 end
 
+function toggle_item(code)
+  local active = Tracker:FindObjectForCode(code).Active
+  code = code.."_hosted"
+  local object = Tracker:FindObjectForCode(code)
+  if object then
+    object.Active = active
+  else
+    if ENABLE_DEBUG_LOG then
+      print(string.format("toggle_item: could not find object for code %s", code))
+    end
+  end
+end
+
 function toggle_hosted_item(code)
   local active = Tracker:FindObjectForCode(code).Active
   code = code:gsub("_hosted", "")
--- scripts/watch.lua
+++ scripts/watch.lua
@@ -76,6 +76,7 @@ HOSTED_ITEMS =
 
 function initialize_watch_items()
   for _, code in pairs(HOSTED_ITEMS) do
+    ScriptHost:AddWatchForCode(code, code, toggle_item)
     ScriptHost:AddWatchForCode(code.."_hosted", code.."_hosted", toggle_hosted_item)
   end
 end
