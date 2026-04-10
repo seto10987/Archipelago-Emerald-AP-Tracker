ad7ae085246bec9cd8090d4c9c17c0d6370057c7
palex00 <palex000000@gmail.com>
2026-04-10 23:43:25 +0200

    Fix a function

--- scripts/autotracking/archipelago.lua
+++ scripts/autotracking/archipelago.lua
@@ -302,7 +302,7 @@ function onBounce(json)
             updateMap(data["mapId"])
         elseif data["type"] == "Encounter" then
             updateEncounter(data["species"], data["slot"], data["encounterType"], data["mapId"])
-            processUnclearedEncounters
+            processUnclearedEncounters()
         end
     end
 end
